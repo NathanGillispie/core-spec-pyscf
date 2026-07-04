cfb587f48c37b2c6e470c3ff046ade66cfae2392
Nathan Gillispie <nwgllspe@memphis.edu>
2026-07-04 15:54:03 -0400

    Added Lazy vs. Eager Gxc parameterizations (All pass).

--- pyscf/qr/test/test_lazy_eager.py
+++ pyscf/qr/test/test_lazy_eager.py
@@ -2,20 +2,19 @@
 
 import numpy
 import pytest
-from pyscf import gto, lib, scf, dft
+from pyscf import gto, dft
 from pyscf.tdscf import RPA, TDA
 
 from pyscf.qr import QR
 from math import isclose
 
-
-@pytest.fixture(scope='module')
-def lih_td():
+@pytest.fixture
+def lih_td(xc, td, frozen_idx):
     mol = gto.M(atom='Li 0 0 0; H 0 0 1.6', basis='def2-svp', verbose=0)
-    mf = dft.RKS(mol, xc='PBE0')
+    mf = dft.RKS(mol, xc=xc)
     mf.grids.level = 1
     mf.kernel()
-    return RPA(mf).run(nstates=4)
+    return td(mf).run(nstates=4, frozen=frozen_idx)
 
 @pytest.fixture
 def lih_eager(lih_td, pair):
@@ -31,11 +30,14 @@ def lih_lazy(lih_td, pair):
     tdip = qrobj.transition_dipole(tdm)
     return float(numpy.linalg.norm(tdip))
 
+@pytest.mark.parametrize('xc', ['PBE0', 'LDA'])
 @pytest.mark.parametrize(
-    'pair',
-     [(0, 1), (0, 3), (1, 3)],
-     ids = ['0-1', '0-3', '1-3']
+    'frozen_idx', [numpy.asarray([]), numpy.asarray([0])],
+    ids=['Freeze[]', 'Freeze[0]'],
+)
+@pytest.mark.parametrize('td', [RPA, TDA], ids=['RPA', 'TDA'])
+@pytest.mark.parametrize(
+    'pair', [(0, 1), (0, 3), (1, 3)], ids=['0-1', '0-3', '1-3'],
 )
 def test_lih(lih_eager, lih_lazy):
     assert isclose(lih_eager, lih_lazy, abs_tol=1e-7)
-
